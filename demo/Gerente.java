54041a6adc655c0201e47cf33ebb61d3ba70afd8
anonimo <anonimo4567890@gmail.com>
2016-09-21 23:47:42 -0700

    Se implementaron metodos set y get para salario gerente

--- demo/Gerente.java
+++ demo/Gerente.java
@@ -1,5 +1,20 @@
-package demo;
+package practica4;
 
 public class Gerente extends Empleado {
+
+    private float SalarioGerente;
+
+    public Gerente(float salario) {
+        super(salario);
+        SalarioGerente = 0;
+    }
+
+    public void setSalarioGerente(float SalarioGerente) {
+        this.SalarioGerente = SalarioGerente;
+    }
+
+    public float getSalarioGerente() {
+        return SalarioGerente;
+    }
 
 }
