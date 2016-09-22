ce909ae0444246faae2d695b8cd50aeb9c3176b2
anonimo <anonimo4567890@gmail.com>
2016-09-21 23:44:53 -0700

    Se implemento set y get en main para las clases Contador, empleado, Gerente

--- demo/Ejemplo.java
+++ demo/Ejemplo.java
@@ -1,12 +1,26 @@
-package demo;
+﻿package practica4;
 
 // Cada archivo .java contiene una clase pública, con el mismo nombre del archivo.
 public class Ejemplo {
 
     // Un programa debe tener un método 'main' como punto de entrada
-    public static void main (String[] args) {
-        Empleado emp = new Empleado();
-        emp.edad = 23;
-        System.out.println(emp.edad);
+    public static void main(String[] args) {
+        
+        Empleado emp = new Empleado(0);
+        Gerente gerente = new Gerente(0);
+        Contador contador = new Contador(0);
+       
+        float salarioBase=100;
+        
+        emp.setEdad(23);
+        emp.setSalario(salarioBase);
+        gerente.setSalario(salarioBase);
+        contador.setSalario(salarioBase);
+        
+        //System.out.println(emp.getEdad());
+        System.out.println("salario de empleado: "+emp.getSalario());
+        System.out.println("salario de contador: "+ ((emp.getSalario())*35/100+contador.getSalario()));
+        System.out.println("salario de gerente : "+ ((emp.getSalario())*50/100+gerente.getSalario()));
+        
     }
 }
