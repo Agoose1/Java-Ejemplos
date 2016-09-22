0f0a9a84a9d24e8a6f0613ba77d8502d998c3f6c
anonimo <anonimo4567890@gmail.com>
2016-09-21 23:48:07 -0700

    Se implementaron metodos set y get para salario contador

--- demo/Contador.java
+++ demo/Contador.java
@@ -1,5 +1,19 @@
-package demo;
+package practica4;
 
 public class Contador extends Empleado {
 
+    private float SalarioContador;
+
+    public Contador(float salario) {
+        super(salario);
+        SalarioContador = 0;
+    }
+
+    public void setSalarioContador(float SalarioContador) {
+        this.SalarioContador = SalarioContador;
+    }
+
+    public float getSalarioContador() {
+        return SalarioContador;
+    }
 }
